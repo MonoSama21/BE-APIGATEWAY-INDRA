5c2353859b30e042777290e8aa10eb05a0e43661
MonoSama21 <yrvin21pachas2004@gmail.com>
2026-02-25 22:07:19 -0500

    Backend - V.06 Mandando cambios viendo si es por las Env - Yrvin Pachas

--- src/main/db/conexion.ts
+++ src/main/db/conexion.ts
@@ -7,14 +7,14 @@ import { FotoCita } from '../models/fotosCitaModel';
 
 
 // Selecciona el schema según el ambiente
-const SCHEMA = process.env.SUPABASE_SCHEMA || 'produccion'; // prod o test
+const SCHEMA = process.env.SUPABASE_SCHEMA || 'produccion'; // prod o desa
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
     host: process.env.SUPABASE_HOST || '',
     port: Number(process.env.SUPABASE_PORT) || 5432,
-    username: 'postgres',
-    password: '123zeusyrayo',
+    username: process.env.SUPABASE_USER || 'postgres',
+    password: process.env.SUPABASE_PASSWORD || '123zeusyrayo',
     database: process.env.SUPABASE_DB || 'postgres',
     schema: SCHEMA,
     entities: [Novio, Cita, RegistroCita, FotoCita],
